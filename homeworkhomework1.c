c8772bb8e89387ec8df0ffb0b4da208399496b6b
Arantza Gizelle Chacón Frías <78566600+ArantzaGizelle@users.noreply.github.com>
2021-02-18 16:55:11 -0600

    Add files via upload

--- homeworkhomework1.c
+++ homeworkhomework1.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main() {
-  float x = 1.842;
-  int y = 15;
-  while (x <= y) {
-    x = x / 3;
-    y = y * x;
-  }
-  printf("%.2f, %d\n", x, y);
+  int i;
+  for (i = 55; i >= 13; i = i - 8);
+  printf("%d\n", i);
 }
